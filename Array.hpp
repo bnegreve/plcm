64299fda210f2d0f3bfa710794277d06b6ae847e
Benjamin Negrevergne <Benjamin.Negrevergne@imag.fr>
2010-07-23 15:10:53 +0200

    Added heads into source files

--- Array.hpp
+++ Array.hpp
@@ -9,28 +9,6 @@
 ** See the README file for installation, usage, and details.
 ** See the LICENSE file for licensing.
 */
-/*
-** PLCM/Melinda
-** (c) Benjamin Negrevergne, 2010
-** Original LCM algorithm from Takaki Uno and Hiroki Arimura.
-**
-** The program was design and implemented in colaboration with
-** Alexandre Termier and Jean-François Méhaut.
-** 
-** See the README file for installation, usage, and details.
-** See the LICENSE file for licensing.
-*/
-/*
-** PLCM/Melinda
-** (c) Benjamin Negrevergne, 2010
-** Original LCM algorithm from Takaki Uno and Hiroki Arimura.
-**
-** The program was design and implemented in colaboration with
-** Alexandre Termier and Jean-François Méhaut.
-** 
-** See the README file for installation, usage, and details.
-** See the LICENSE file for licensing.
-*/
 
 #ifndef   	_ARRAY_HPP_
 #define   	_ARRAY_HPP_
